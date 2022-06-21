aba750f074badc769b1ceaac20f1ada89d3dc2c3
viewcommz <yangmae@viewcommz.com>
2022-06-21 15:31:47 +0900

    FIX: 배포테스트

---
+++ app.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "./build")));
+
+app.get("/", (req, res) => {
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`port ${PORT}`));
